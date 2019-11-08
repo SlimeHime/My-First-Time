b17a4dfb91c5a0b351d4ccaad7f704934607bb39
SlimeHime <Solak2002@googlemail.com>
2019-11-08 20:05:36 +0100

    javascript add

---
+++ main.js
@@ -0,0 +1,9 @@
+var outputField=document.getElementById("output")
+
+var inputField=document.getElementById("input")
+console.log(inputField.value)
+
+function updateOutput(){
+    console.log("oh fuck i can't believe you've done this")
+    outputField.innerHTML=inputField.value
+}
